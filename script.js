917d5d136265e6007bf30aaf37400d6011dd5369
Gelson Martinhago <gelson.martinhago@gmail.com>
2022-08-09 20:57:20 -0300

    Finalizada a funcionalidade de hello world

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-document.querySelectorAll('clique-aqui').click = () => {
-    const nome = document.querySelectorAll('#campo-nome').values;
+document.querySelector('#clique-aqui').onclick = () => {
+    const nome = document.querySelector('#campo-nome').value;
+    if(nome){
+        alert(`hello world, ${nome}!`);
+    }
 };
   
